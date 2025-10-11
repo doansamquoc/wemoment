92467853eaf558f17f87f7b0431c0aaf4e3bf2e2
Sam <doansamquoc@gmail.com>
2025-10-11 22:00:42 +0700

    First commit

---
+++ src/main/java/com/doansamquoc/wemoment/WemomentApplication.java
@@ -0,0 +1,13 @@
+package com.doansamquoc.wemoment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemomentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemomentApplication.class, args);
+	}
+
+}
