3e359934aa814a6b969d195e1e9bf00a09dcfe65
Sam <doansamquoc@gmail.com>
2025-10-15 09:05:47 +0700

    Implement UserServiceImpl to handle user creation with validation for unique username and email; include role and provider assignment.

--- src/main/java/com/doansamquoc/wemoment/user/service/UserService.java
+++ src/main/java/com/doansamquoc/wemoment/user/service/UserService.java
@@ -1,52 +1,11 @@
 package com.doansamquoc.wemoment.user.service;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.stereotype.Service;
 
-import com.doansamquoc.wemoment.common.enums.AuthProvider;
-import com.doansamquoc.wemoment.common.enums.ErrorCode;
-import com.doansamquoc.wemoment.common.enums.Role;
-import com.doansamquoc.wemoment.common.exception.AppException;
 import com.doansamquoc.wemoment.user.dto.UserCreationRequest;
 import com.doansamquoc.wemoment.user.dto.UserResponse;
-import com.doansamquoc.wemoment.user.entity.User;
-import com.doansamquoc.wemoment.user.mapper.UserMapper;
-import com.doansamquoc.wemoment.user.repository.UserRepository;
-
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 
 @Service
-@RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class UserService {
-    UserRepository userRepository;
-    UserMapper userMapper;
-
-    public UserResponse createUser(UserCreationRequest request) {
-        if (userRepository.existsByUsername(request.getUsername())) {
-            throw new AppException(ErrorCode.USERNAME_ALREADY_EXISTS);
-        }
-        if (userRepository.existsByEmailAddress(request.getEmailAddress())) {
-            throw new AppException(ErrorCode.EMAIL_ALREADY_EXISTS);
-        }
-
-        User user = userMapper.toUserCreationRequest(request);
-
-        Set<Role> roles = new HashSet<>();
-        roles.add(Role.USER);
-
-        Set<AuthProvider> providers = new HashSet<>();
-        providers.add(AuthProvider.LOCAL);
-
-        user.setRoles(roles);
-        user.setProviders(providers);
-
-        userRepository.save(user);
-
-        return userMapper.toUserResponse(user);
-    }
+public interface UserService {
+    public UserResponse createUser(UserCreationRequest request);
 }
---
+++ src/main/java/com/doansamquoc/wemoment/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.doansamquoc.wemoment.user.service.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.doansamquoc.wemoment.common.enums.AuthProvider;
+import com.doansamquoc.wemoment.common.enums.ErrorCode;
+import com.doansamquoc.wemoment.common.enums.Role;
+import com.doansamquoc.wemoment.common.exception.AppException;
+import com.doansamquoc.wemoment.user.dto.UserCreationRequest;
+import com.doansamquoc.wemoment.user.dto.UserResponse;
+import com.doansamquoc.wemoment.user.entity.User;
+import com.doansamquoc.wemoment.user.mapper.UserMapper;
+import com.doansamquoc.wemoment.user.repository.UserRepository;
+import com.doansamquoc.wemoment.user.service.UserService;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserServiceImpl implements UserService {
+    UserRepository userRepository;
+    UserMapper userMapper;
+
+    @Override
+    public UserResponse createUser(UserCreationRequest request) {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new AppException(ErrorCode.USERNAME_ALREADY_EXISTS);
+        }
+        if (userRepository.existsByEmailAddress(request.getEmailAddress())) {
+            throw new AppException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        }
+
+        User user = userMapper.toUserCreationRequest(request);
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(Role.USER);
+
+        Set<AuthProvider> providers = new HashSet<>();
+        providers.add(AuthProvider.LOCAL);
+
+        user.setRoles(roles);
+        user.setProviders(providers);
+
+        userRepository.save(user);
+
+        return userMapper.toUserResponse(user);
+    }
+}
